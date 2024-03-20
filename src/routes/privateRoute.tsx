4523cdf7115677352fd6ceafef4d4837644d8ba0
Legion <lhgia.huyy@gmail.com>
2024-03-20 19:31:44 +0700

    fix conflict

--- src/routes/privateRoute.tsx
+++ src/routes/privateRoute.tsx
@@ -1,32 +1,31 @@
 import { Navigate } from "react-router-dom";
 import { useAppSelector } from "../redux/hook";
 interface PrivateRouteProps {
-    inverted: boolean;
-    children: React.ReactNode;
-    requiredRoles?: string[];
+  inverted: boolean;
+  children: React.ReactNode;
+  requiredRoles?: string[];
 }
 
-const PrivateRoute = ({ inverted, children, requiredRoles }: PrivateRouteProps) => {
-    const access_token = localStorage.getItem("access_token");
-    const isAuth = access_token ? true : false;
-    const { role: currentRole } = useAppSelector((state) => state.roleCheck);
-    // const user = localStorage.getItem('user');
-    // const userObj = user ? JSON.parse(user) : {};
-    // const isFirstLogin = userObj.user.isFirstLogin;
+const PrivateRoute = ({
+  inverted,
+  children,
+  requiredRoles,
+}: PrivateRouteProps) => {
+  const access_token = localStorage.getItem("access_token");
+  const isAuth = access_token ? true : false;
+  // const user = localStorage.getItem('user');
+  // const userObj = user ? JSON.parse(user) : {};
+  // const isFirstLogin = userObj.user.isFirstLogin;
 
-    // if (isFirstLogin && window.location.pathname !== '/account/change-password') {
-    //   return <Navigate to='/account/change-password' />;
-    // }
+  // if (isFirstLogin && window.location.pathname !== '/account/change-password') {
+  //   return <Navigate to='/account/change-password' />;
+  // }
 
-    if (inverted) {
-        return isAuth ? <Navigate to="/Event" /> : children;
-    }
+  if (inverted) {
+    return isAuth ? <Navigate to="/Event" /> : children;
+  }
 
-    if (currentRole.role && !requiredRoles?.some((r) => currentRole.role === r)) return (<div></div>)
-
-        ;
-
-    return isAuth ? children : <Navigate to="/login" />;
+  return isAuth ? children : <Navigate to="/login" />;
 };
 
 export default PrivateRoute;
