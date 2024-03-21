100128ddfd929cea0be8b50892bb82351cea6f10
Legion <lhgia.huyy@gmail.com>
2024-03-21 17:52:42 +0700

    fix env.ts

--- src/utils/env.ts
+++ src/utils/env.ts
@@ -1,6 +1,6 @@
 export const Envs = {
     apiLocal: "https://swdbirthdaypartybooking.somee.com/",
-    apiRemote: "https://swdbirthdaypartybooking.somee.com",
+    apiRemote: "https://swdbirthdaypartybooking.somee.com/",
     apiEnv: "https://swdbirthdaypartybooking.somee.com/",
   };
   
